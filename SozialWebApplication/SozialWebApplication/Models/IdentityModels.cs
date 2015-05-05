10a6dae4bf8e24244d872477468b5f0b8cb28a3a
perlahj <perlahj@gmail.com>
2015-05-05 20:46:16 +0000

    IdentityUser: LineOfStudy, FullName

--- SozialWebApplication/SozialWebApplication/Models/IdentityModels.cs
+++ SozialWebApplication/SozialWebApplication/Models/IdentityModels.cs
@@ -5,7 +5,8 @@ namespace SozialWebApplication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-		
+		public string FullName {get; set;}
+		public string LineOfStudy {get; set;}
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
