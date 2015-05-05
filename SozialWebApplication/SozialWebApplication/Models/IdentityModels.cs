890e989f9b134b7f60d8f00c5959939a5fa68cea
perlahj <perlahj@gmail.com>
2015-05-05 20:32:12 +0000

    Post, Comment, Group Entity Classes

--- SozialWebApplication/SozialWebApplication/Models/IdentityModels.cs
+++ SozialWebApplication/SozialWebApplication/Models/IdentityModels.cs
@@ -5,7 +5,7 @@ namespace SozialWebApplication.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-		// This is only for testing and can be deleted -Perla
+		
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
