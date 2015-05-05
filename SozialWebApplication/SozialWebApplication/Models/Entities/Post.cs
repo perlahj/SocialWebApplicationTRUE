4fd1b05f6b39b4dc5ca2c74f91be502b49e1732f
perlahj <perlahj@gmail.com>
2015-05-05 22:10:15 +0000

    DbSet, breytingar a entity

---
+++ SozialWebApplication/SozialWebApplication/Models/Entities/Post.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SozialWebApplication.Models
+{
+	public class Post
+	{
+		public int Id { get; set; }
+		public string UserName { get; set; }
+		public DateTime DateCreated { get; set; }
+		public string Body { get; set; }
+		public int Like { get; set; }
+	}
+}
