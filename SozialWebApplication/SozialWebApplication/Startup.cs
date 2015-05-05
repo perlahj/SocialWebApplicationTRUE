a03c5694a94ac1954494b9a05b482b8b9696d3ad
perlahj <perlahj@gmail.com>
2015-05-05 17:36:07 +0000

    Empty Project

---
+++ SozialWebApplication/SozialWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SozialWebApplication.Startup))]
+namespace SozialWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
