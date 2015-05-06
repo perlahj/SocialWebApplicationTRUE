843533d34aa6d9832122aeac5e6269e89e04e0b4
perlahj <perlahj@gmail.com>
2015-05-06 16:43:07 +0000

    FolloweConnection Class

---
+++ SozialWebApplication/SozialWebApplication/Models/Entities/FollowerConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SozialWebApplication.Models.Entities
+{
+	public class FollowerConnection
+	{
+		public int Id { get; set; }
+		public string UserFollowing { get; set; }
+		public string UserToFollow { get; set; }
+	}
+}
