b1b9a1f9b761632485abf78ca3ab6920784c23a1
perlahj <perlahj@gmail.com>
2015-05-06 16:34:18 +0000

    Bara comment

--- SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
@@ -23,12 +23,9 @@ namespace SozialWebApplication.Controllers
 		public ActionResult About()
 		{
 			ViewBag.Message = "Your application description page.";
-			/*var userService = new UserService();
-			var newFullName = "Perla Osk";
-			var userName = User.Identity.Name;
-			userService.ChangeFullName(userName, newFullName); */
-			var userService = new UserService();
-			userService.ChangeLineOfStudy(User.Identity.Name, "Computer Science");
+			// var userService = new UserService();
+			// userService.ChangeFullName(User.Identity.Name, "Perla Osk");
+			// userService.ChangeLineOfStudy(User.Identity.Name, "Computer Science");
 			 
 			return View();
 		}
