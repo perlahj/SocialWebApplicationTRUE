ce08ba0401338f810154e88ea500913ca84ed02f
stefanakristin <stefana11@ru.is>
2015-05-06 10:52:07 +0000

    test

--- SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+//klklkl
 namespace SozialWebApplication.Controllers
 {
 	public class HomeController : Controller
