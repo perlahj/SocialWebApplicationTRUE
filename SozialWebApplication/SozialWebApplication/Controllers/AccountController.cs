5cab5d0fc66f952ff3fd005ed20d02ea7039cd60
Karlotta14 <karlotta14@ru.is>
2015-05-06 13:35:32 +0000

    fsaf

--- SozialWebApplication/SozialWebApplication/Controllers/AccountController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Owin.Security;
 using SozialWebApplication.Models;
-//Karlotta
+//Karlott
 
 namespace SozialWebApplication.Controllers
 {
