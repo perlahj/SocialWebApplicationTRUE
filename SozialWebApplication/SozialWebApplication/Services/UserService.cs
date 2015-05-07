4fa03d1035844326c9e1c6e9c4706ab2822cb817
perlahj <perlahj@gmail.com>
2015-05-07 13:53:31 +0000

    AddMatch, AddFollow, RemoveFollow

--- SozialWebApplication/SozialWebApplication/Services/UserService.cs
+++ SozialWebApplication/SozialWebApplication/Services/UserService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SozialWebApplication.Models;
+using SozialWebApplication.Models.Entities;
 
 namespace SozialWebApplication.Services
 {
@@ -27,5 +28,42 @@ namespace SozialWebApplication.Services
 			userWithUserName.LineOfStudy = newLineOfStudy;
 			db.SaveChanges();
 		}
+
+		// Has not been tested
+		public void AddNewFollow(string userFollowing, string userToFollow)
+		{
+			FollowerConnection fc = new FollowerConnection();
+			fc.UserFollowing = userFollowing;
+			fc.UserToFollow = userToFollow;
+			db.FollowerConnections.Add(fc);
+			db.SaveChanges();
+		}
+	 
+		// Has not been tested
+		public void RemoveFollow(string userFollowing, string userToFollow)
+		{
+			var followerConnection = (from dc in db.FollowerConnections
+									  where dc.UserFollowing == userFollowing &&
+									  dc.UserToFollow == userToFollow
+									  select dc).FirstOrDefault();
+			db.FollowerConnections.Remove(followerConnection);
+			db.SaveChanges();
+		}
+		
+		public void AddMatch(string userMatching, string userMatched)
+		{
+			MatchConnection mc = new MatchConnection();
+			mc.UserMatching = userMatching;
+			mc.UserMatched = userMatched;
+			db.MatchConnections.Add(mc);
+			db.SaveChanges();
+		}
+
+		/*public void RemoveMatch(string userRemoving, string userRemoved)
+		{
+			var matchConnection = (from mc in db.MatchConnections
+									   where mc.UserMatching == userRemoved &&
+									   mc.UserMatched)
+		}  */
 	}
 }
