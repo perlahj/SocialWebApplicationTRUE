91cb6fae69462ffdd9a3bc4bca839c0cac68f592
astagis <astagis@gmail.com>
2015-05-07 18:56:25 +0100

    bjo til other user profile application view model

---
+++ SozialWebApplication/SozialWebApplication/ViewModel/OtherUserProfileViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SozialWebApplication.ViewModel
+{
+    public class OtherUserProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string LineOfStudy { get; set; }
+        public bool IsFollowing { get; set; }
+        public bool HasMatched { get; set; }
+        public bool IsDoubleMatch { get; set; }
+    }
+}
