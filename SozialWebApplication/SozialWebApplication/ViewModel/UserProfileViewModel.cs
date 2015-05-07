ecd799f1de21b04e5799e7423a4872c1b54bb919
astagis <astagis@gmail.com>
2015-05-07 18:50:14 +0100

    bjo til user profile viw model

---
+++ SozialWebApplication/SozialWebApplication/ViewModel/UserProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SozialWebApplication.Models;
+using SozialWebApplication.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SozialWebApplication.ViewModel
+{
+    public class UserProfileViewModel
+    {
+        public string FullName { get; set; }
+        public string LineOfStudy { get; set; }
+        public List<ApplicationUser> Followings { get; set; }
+        public List<ApplicationUser> DoubleMatches { get; set; }
+        public List<Group> GroupsJoined { get; set; }
+    }
+}
