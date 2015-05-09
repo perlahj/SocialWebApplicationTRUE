175619a9e40ff0fa36b64a0e5fa0aa8e9b78ea77
Karlotta14 <karlotta14@ru.is>
2015-05-09 14:57:03 +0000

    JavaScript byrjun

---
+++ SozialWebApplication/SozialWebApplication/Scripts/aWholeLottaJavascript.js
@@ -0,0 +1,12 @@
+﻿$(".my-name a").click(function () {
+    alert("veiiii");
+    event.preventDefault();
+    
+    $.ajax({
+        url: "test.html",
+    }).success(function (data) {
+        $(".namecard").html();
+        $(this).addClass("done");
+    });
+
+});
