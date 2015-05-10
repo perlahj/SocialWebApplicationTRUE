ee8569321e25d0a53f0c7892f68c0a5f7f316d2d
perlahj <perlahj@gmail.com>
2015-05-10 20:51:17 +0000

    Breyta HomeController eins og hann er í master en taka út Ástu

--- SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
@@ -3,17 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SozialWebApplication.Services;
-using SozialWebApplication.Models;
-using SozialWebApplication.Models.Entities;
-using SozialWebApplication.ViewModel;
-
+// Stefana
+// Perla - Test
+// Asta 
+// Karlotta
+// Audur
 namespace SozialWebApplication.Controllers
 {
 	public class HomeController : Controller
 	{
-		
-		public ActionResult Index()
+		public ActionResult Banner()
 		{
 			return View();
 		}
@@ -31,60 +30,5 @@ namespace SozialWebApplication.Controllers
 
 			return View();
 		}
-
-        public ActionResult Group(string? s)
-        {
-            GroupViewModel model = new GroupViewModel();
-
-            // ATH: Smida fall sem saekir allt
-            //Group g = Initialize(s);
-            
-            model.GroupName = g.GroupName;
-            
-            // smida fall sem saekir post listann
-            //model.GroupPost = g.GroupPost;
-
-            return View(model);
-        }
-
-        public ActionResult OtherUserProfile(string? s)
-        {
-            OtherUserProfileViewModel model = new OtherUserProfileViewModel();
-            
-            // Smida fall sem saekir allar uppl sem tharf her
-            //ApplicationUser u = Initialize(s);
-            model.FullName = u.FullName;
-            model.LineOfStudy = u.LineOfStudy;
-            
-            model.Email = u.Email;
-
-            return View(model);
-        }
-        
-        public ActionResult UserProfile(string? s)
-        {
-            UserProfileViewModel model = new UserProfileViewModel();
-            
-            // Smida fall sem saekir allar uppl
-            //ApplicationUser u = Initialize(s);
-            model.FullName = u.FullName;
-            model.LineOfStudy = u.LineOfStudy;
-
-            model.Email = u.Email;
-
-            return View(model);
-        }
-
-        public ActionResult Comment(string? s)
-        {
-            CommentViewModel model = new CommentViewModel();
-            
-            // Smida fall sem saekir allar uppl sem tharf
-            //Comment c = Initialize(s);
-            model.UserName = c.UserName;
-            model.Body = c.Body;
-
-            return View(model);
-        }
 	}
 }
