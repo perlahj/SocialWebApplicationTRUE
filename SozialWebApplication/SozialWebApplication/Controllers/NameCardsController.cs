fe7cde9a2de12d2611d5eb0eb4e2607368d8c7f1
perlahj <perlahj@gmail.com>
2015-05-11 17:56:50 +0000

    delete comments

--- SozialWebApplication/SozialWebApplication/Controllers/NameCardsController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/NameCardsController.cs
@@ -30,7 +30,6 @@ namespace SozialWebApplication.Controllers
 
         public ActionResult OthersNameCard(string otherUser)
         {
-			//  tst
 			nameCardVM.userWithId = us.GetUserByUserName(otherUser);
 			
 			return View(nameCardVM);
@@ -41,8 +40,6 @@ namespace SozialWebApplication.Controllers
 		[HttpGet]
 		public ActionResult EditNameCard()
 		{
-			//NameCardViewModel nameCardVM = new NameCardViewModel();
-			//nameCardVM.userWithId = UserService.Instance.GetUserByUserName(User.Identity.Name);
 			nameCardVM.userWithId = us.GetUserByUserName(User.Identity.Name);
 
 			return View(nameCardVM);
@@ -63,13 +60,6 @@ namespace SozialWebApplication.Controllers
 
 			return View(nameCardVM);
         }
-
-		/*public ActionResult Search(FormCollection collection)
-        {
-			nameCardVM.AllUsers = us.GetAllUsers();
-			nameCardVM.SearchResultsUsers = us.SearchAllUsers(collection.Get(""));
-			return View(nameCardVM);
-        }*/
 
 		public ActionResult Search()
 		{
