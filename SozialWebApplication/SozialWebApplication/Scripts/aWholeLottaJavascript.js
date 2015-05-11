6e337a151faac2a0384f7406d2be0490186f2a7a
Karlotta14 <karlotta14@ru.is>
2015-05-11 12:31:00 +0000

    Java

--- SozialWebApplication/SozialWebApplication/Scripts/aWholeLottaJavascript.js
+++ SozialWebApplication/SozialWebApplication/Scripts/aWholeLottaJavascript.js
@@ -1,12 +1,14 @@
 ﻿$(".my-name a").click(function () {
     alert("veiiii");
     event.preventDefault();
-    
+
     $.ajax({
         url: "OwnNameCards.cshtml",
     }).success(function (data) {
+        $(".feed-body").empty();
         $(".namecard").html();
         $(this).addClass("done");
     });
 
+    
 });
