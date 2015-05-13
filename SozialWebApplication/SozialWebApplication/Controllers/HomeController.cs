3442dbc7f5ed057834eea616dc75474e3a882f81
perlahj <perlahj@gmail.com>
2015-05-13 12:01:14 +0000

    Default post when creating group.

--- SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
+++ SozialWebApplication/SozialWebApplication/Controllers/HomeController.cs
@@ -78,20 +78,6 @@ namespace SozialWebApplication.Controllers
 			return PartialView("~/Views/Home/NewsfeedGroups.cshtml", groupVM);
 		}
 
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
-		}
-
 		public ActionResult SearchGroups()
 		{
 			nameCardVM.AllUserGroups = gs.GetAllGroupsForUser(User.Identity.Name);
@@ -115,7 +101,12 @@ namespace SozialWebApplication.Controllers
 			string groupName = collection.Get("newgroup-name");
 			gs.AddNewGroup(groupName);
 			int groupId = gs.GetGroupIdbyName(groupName);
+			// Register user to the new group.
 			gs.AddUserToGroup(groupId, User.Identity.Name);
+			// Post to the newly created group.
+			string postBody = User.Identity.Name + " created the group " + groupName + "!";
+			
+			ps.AddNewPost(User.Identity.Name, groupId, postBody);
 			
 			nameCardVM.AllUserGroups = gs.GetAllGroupsForUser(User.Identity.Name);
 			nameCardVM.AllGroups = gs.GetAllGroups();
